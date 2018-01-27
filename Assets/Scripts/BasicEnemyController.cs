9d35bce93c6ca365757ea8878e094df9b5390a3e
micantre <micantre@calpoly.edu>
2018-01-26 19:01:37 -0800

    Simple pathing implemented

--- Assets/Scripts/BasicEnemyController.cs
+++ Assets/Scripts/BasicEnemyController.cs
@@ -4,16 +4,67 @@ using UnityEngine;
 
 public class BasicEnemyController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform path;
+    public float speed = 2;
+    public float waitTime = 3;
+    public float turnSpeed = 50;
 
-    private void FixedUpdate() {
-       
+    void Start()
+    {
+        Vector3[] waypoints = new Vector3[path.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = path.GetChild(i).position;
+        }
+
+        StartCoroutine(FollowPath(waypoints));
+    }
+
+    IEnumerator FollowPath(Vector3[] waypoints)
+    {
+        transform.position = waypoints[0];
+
+        int targetWaypointIndex = 0;
+        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+        transform.LookAt(targetWaypoint);
+
+        while (true)
+        {
+			transform.position = Vector2.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+            if (transform.position == targetWaypoint)
+            {
+                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+                targetWaypoint = waypoints[targetWaypointIndex];
+                yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine(TurnToFace(targetWaypoint));
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        Vector3 dirToLookTarget = (lookTarget - transform.position).normalized;
+        float targetAngle = 90 - Mathf.Atan2(dirToLookTarget.x, dirToLookTarget.y) * Mathf.Rad2Deg;
+
+        while (Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.z, targetAngle)) > 0.05f)
+        {
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngle, turnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.forward * angle;
+            yield return null;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 startPosition = path.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach (Transform waypoint in path)
+        {
+            Gizmos.DrawSphere(waypoint.position, 0.5f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
     }
 }
