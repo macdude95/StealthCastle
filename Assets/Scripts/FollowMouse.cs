4639e4b673d8f41ada9a8504fbc26f908e8ab9d3
micantre <micantre@calpoly.edu>
2018-01-28 02:55:29 -0800

    cleaned up the lighting code and sample scene

--- Assets/Scripts/FollowMouse.cs
+++ Assets/Scripts/FollowMouse.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowMouse : MonoBehaviour {
-
-	public Camera c;
 	
 	// Update is called once per frame
 	void Update () {
+		Camera c = Camera.current;
 		transform.position = new Vector2(c.ScreenToWorldPoint(Input.mousePosition).x, c.ScreenToWorldPoint(Input.mousePosition).y);
 	}
 }
