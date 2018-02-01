47b5da9b4cfaba62ba2c62afc8f74b9d490fdd16
Daniel Anderson <dander24@calpoly.edu>
2018-02-01 15:34:52 -0800

    Dynamic Patrols

---
+++ Assets/PathNodeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNodeController : MonoBehaviour {
+
+    public GameObject nextNode;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(this.transform.position, nextNode.transform.position, Color.cyan);
+    }
+
+    public GameObject getNextNode() { return nextNode; }
+}
--- Assets/Scripts/BasicEnemyController.cs
+++ Assets/Scripts/BasicEnemyController.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class BasicEnemyController : MonoBehaviour {
 
+    //state machine states
+    public static readonly int STATE_PATHING = 0, STATE_ALERT = 1, STATE_HUNTING = 2;
+
+
+    //pathfinding controller
+    public GameObject pathNode;
+    private AIPath pathController;
+    private Seeker seeker;
+
+
     public void Start()
     {
+        pathController = this.GetComponent<AIPath>();
+        InitPath();
     }
 
-    public void Update()
+    public void FixedUpdate()
     {
+        if(!pathController.pathPending && pathController.reachedEndOfPath)
+        {
+            UpdateDestination();
+        }
+    }
 
+    //update destination based on current state
+    private void UpdateDestination()
+    {
+        pathNode = pathNode.GetComponent<PathNodeController>().getNextNode();
+        pathController.destination = pathNode.transform.position;
+        pathController.SearchPath();
+    }
+
+    private void InitPath()
+    {
+        //move to self, to kick off pathfinding
+        pathController.destination = pathNode.transform.position;
+        pathController.SearchPath();
     }
 
 }
