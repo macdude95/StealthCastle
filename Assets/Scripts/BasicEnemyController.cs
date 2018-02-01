9bee4fa84979a65ed3188e25ebd4a743814f6dc5
Daniel Anderson <dander24@calpoly.edu>
2018-02-01 10:33:27 -0800

    fixed merge
    i hate git sometimes

--- Assets/Scripts/BasicEnemyController.cs
+++ Assets/Scripts/BasicEnemyController.cs
@@ -4,67 +4,30 @@ using UnityEngine;
 
 public class BasicEnemyController : MonoBehaviour {
 
-    public Transform path;
-    public float speed = 2;
-    public float waitTime = 3;
-    public float turnSpeed = 50;
+    Vector3 target;
+    public float speed = 0.01f;
+    Vector2 velocity;
 
-    void Start()
+    public void Start()
     {
-        Vector3[] waypoints = new Vector3[path.childCount];
-        for (int i = 0; i < waypoints.Length; i++)
-        {
-            waypoints[i] = path.GetChild(i).position;
-        }
-
-        StartCoroutine(FollowPath(waypoints));
+        target = this.transform.position;
     }
 
-    IEnumerator FollowPath(Vector3[] waypoints)
+    public void Update()
     {
-        transform.position = waypoints[0];
-
-        int targetWaypointIndex = 0;
-        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
-        transform.LookAt(targetWaypoint);
-
-        while (true)
+        if(this.transform.position != target)
         {
-			transform.position = Vector2.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
-            if (transform.position == targetWaypoint)
-            {
-                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
-                targetWaypoint = waypoints[targetWaypointIndex];
-                yield return new WaitForSeconds(waitTime);
-                yield return StartCoroutine(TurnToFace(targetWaypoint));
-            }
-            yield return null;
+           this.transform.position = Vector2.MoveTowards(this.transform.position, target, speed);         
         }
     }
 
-    IEnumerator TurnToFace(Vector3 lookTarget)
+    public void MoveToSeenPosition(Vector3 coord)
     {
-        Vector2 dirToLookTarget = (lookTarget - transform.position).normalized;
-        float targetAngle = 90 - Mathf.Atan2(dirToLookTarget.x, dirToLookTarget.y) * Mathf.Rad2Deg;
-
-        while (Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.z, targetAngle)) > 0.05f)
-        {
-            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngle, turnSpeed * Time.deltaTime);
-            transform.eulerAngles = Vector3.forward * angle;
-            yield return null;
-        }
+        target = coord;
+        var dir = target - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        
     }
 
-    void OnDrawGizmos()
-    {
-        Vector3 startPosition = path.GetChild(0).position;
-        Vector3 previousPosition = startPosition;
-        foreach (Transform waypoint in path)
-        {
-            Gizmos.DrawSphere(waypoint.position, 0.5f);
-            Gizmos.DrawLine(previousPosition, waypoint.position);
-            previousPosition = waypoint.position;
-        }
-        Gizmos.DrawLine(previousPosition, startPosition);
-    }
 }
