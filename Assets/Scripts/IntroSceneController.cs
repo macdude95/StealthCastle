1d01255bcf1d0b63073c55c011505da98d676a87
micantre <micantre@calpoly.edu>
2018-03-06 18:11:15 -0800

    started animating intro

---
+++ Assets/Scripts/IntroSceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSceneController : MonoBehaviour {
+
+    public Animator goodKingAnimator;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        goodKingAnimator.ResetTrigger("NEXT");
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            goodKingAnimator.SetTrigger("NEXT");
+        }
+	}
+}
