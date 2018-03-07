4a512c83dbd1bee5ee242a391814941f1efc4785
micantre <micantre@calpoly.edu>
2018-03-06 20:34:05 -0800

    more of the intro cutscene complete

---
+++ Assets/Scripts/IntroScripts/GoodKingController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* GoodKingController.cs
+ * Created by Michael Cantrell
+ */
+
+public class GoodKingController : MonoBehaviour {
+
+    private void DidFinishWalking() {
+        IntroSceneController.instance.NextDialogue();
+    }
+
+    private void DidStartWalking()
+    {
+        IntroSceneController.instance.NextDialogue();
+    }
+}
---
+++ Assets/Scripts/IntroScripts/IntroSceneController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* IntroSceneController.cs
+ * Created by Michael Cantrell
+ */
+
+public class IntroSceneController : MonoBehaviour {
+
+    public Animator goodKingAnimator;
+    public GameObject finishTrigger;
+    public Text dialogueText;
+    public static IntroSceneController instance;
+    public GameObject lockCharacterObject;
+
+    private int dialogueIndex = 0;
+    private static readonly string[] dialogue = { 
+        "Good King: I'm about to go and meet with the bad king.", 
+        "",
+        "???: *screams*", 
+        "Corrects", 
+        "Wrongs" };
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+	void Start () {
+        // don't let the player leave until the cutscene is finished
+        //finishTrigger.SetActive(false);
+        NextDialogue();
+	}
+	
+	void Update () {
+        goodKingAnimator.ResetTrigger("NEXT");
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            goodKingAnimator.SetTrigger("NEXT");
+
+        }
+        if (dialogueIndex > 1)
+        {
+            lockCharacterObject.SetActive(false);
+        }
+	}
+
+    public void NextDialogue() {
+        if (dialogueIndex >= dialogue.Length) {
+            dialogueText.text = "";
+        } else if (dialogue[dialogueIndex].Equals("")) {
+            dialogueText.text = "";
+            dialogueIndex++;
+        } else {
+            dialogueText.text = dialogue[dialogueIndex++] + "\n(Press Space To Advance)";
+        }
+    }
+}
