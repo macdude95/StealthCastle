ad19ad98740f22f844e2d2e3a9fc0fe40bd76ba4
micantre <micantre@calpoly.edu>
2018-03-20 01:02:44 -0700

    more complete final level cut scene thingy

---
+++ Assets/Scripts/EndSceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndSceneController : MonoBehaviour {
+
+    public Animator kingAnimator;
+    public Collider2D inRangeTrigger;
+    public Collider2D playerBoxCollider;
+    public string endCreditsSceneName;
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerBoxCollider.IsTouching(inRangeTrigger) && Input.GetKeyDown(KeyCode.P)) {
+            endGame();
+        }
+	}
+
+    private void endGame() {
+        kingAnimator.SetTrigger("DIE");
+        GameController.instance.LoadNewLevel(endCreditsSceneName);
+    }
+}
