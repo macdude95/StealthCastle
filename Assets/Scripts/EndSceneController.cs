a88d92d8fe1e3f5551c0017261643312a4f1c17f
micantre <micantre@calpoly.edu>
2018-03-20 01:20:19 -0700

    you can now kill the king, and the game tells you how

--- Assets/Scripts/EndSceneController.cs
+++ Assets/Scripts/EndSceneController.cs
@@ -1,23 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndSceneController : MonoBehaviour {
 
     public Animator kingAnimator;
     public Collider2D inRangeTrigger;
     public Collider2D playerBoxCollider;
+    public Text pressSpaceToKill;
     public string endCreditsSceneName;
-	
+    private bool gameEnded = false;
+
+	private void Awake()
+	{
+        pressSpaceToKill.gameObject.SetActive(false);
+	}
+
 	// Update is called once per frame
 	void Update () {
-        if (playerBoxCollider.IsTouching(inRangeTrigger) && Input.GetKeyDown(KeyCode.P)) {
-            endGame();
+        if (playerBoxCollider.IsTouching(inRangeTrigger) && !gameEnded){
+            pressSpaceToKill.gameObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Space)) 
+            {
+
+                gameEnded = true;
+                StartCoroutine(endGame());
+            }
+        } else {
+            pressSpaceToKill.gameObject.SetActive(false);
         }
 	}
 
-    private void endGame() {
+    IEnumerator endGame() {
         kingAnimator.SetTrigger("DIE");
+        yield return new WaitForSeconds(2);
         GameController.instance.LoadNewLevel(endCreditsSceneName);
     }
 }
